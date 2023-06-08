32576e31b4e163b8c6d15f643351d0e55286f04b
HyperAlch <Daemonleak@protonmail.com>
2023-06-08 18:15:22 -0400

    shell.nix cleanup

--- shell.nix
+++ shell.nix
@@ -3,29 +3,16 @@ clangStdenv.mkDerivation {
   name = "rust-sqlless-nix-shell";
   
   nativeBuildInputs = with xorg; [
-    libxcb
-    libXcursor
-    libXrandr
-    libXi
     pkg-config
   ] ++ [
     python3
-    libGL
-    libGLU
   ];
 
   buildInputs = [ 
     llvm
-    xorg.libX11
-    wayland
-    trunk
     openssl
-    nodePackages.tailwindcss
     libxkbcommon
   ];
 
   LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-  shellHook = ''
-      export LD_LIBRARY_PATH=/run/opengl-driver/lib/:${lib.makeLibraryPath ([libGL libGLU])}
-  '';
 }
